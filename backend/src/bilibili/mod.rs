2031095826cff0efd879fd15c51597aec5b3d14b
HaoCheng <ch1994@outlook.com>
2023-01-03 12:30:52 +0800

    update bilibili api
    Bump to 0.2.3

--- backend/src/bilibili/mod.rs
+++ backend/src/bilibili/mod.rs
@@ -1,4 +1,4 @@
-use anyhow::Context;
+use anyhow::{Context, Ok};
 use async_trait::async_trait;
 use serde_json::json;
 
@@ -13,7 +13,8 @@ pub struct StreamRoom {
 
 impl StreamRoom {
     pub fn new(room_id: u64, client: reqwest::Client, qn_data: u64) -> Self {
-        let api_url = "https://api.live.bilibili.com/room/v1/Room/playUrl".to_owned();
+        let api_url =
+            "https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo".to_owned();
         Self {
             room_id,
             client,
@@ -27,9 +28,11 @@ impl StreamRoom {
 impl GetUrls for StreamRoom {
     async fn get_urls(&self) -> anyhow::Result<Vec<String>> {
         let query = json!({
-            "cid": self.room_id,
+            "room_id": self.room_id,
+            "format": "0,1,2",
+            "protocol": "0,1",
+            "codec": "0,1",
             "qn": self.qn_data,
-            "platform": "h5"
         });
         let res = self
             .client
@@ -39,13 +42,30 @@ impl GetUrls for StreamRoom {
             .await?
             .json::<serde_json::Value>()
             .await?;
-        let urls = res["data"]["durl"]
+        let res = res["data"]["playurl_info"]["playurl"]["stream"]
             .as_array()
-            .context(res.to_string())?
+            .context("Streaming is not started")?
             .iter()
-            .flat_map(|f| f["url"].as_str().map(|f| f.to_owned()))
+            .flat_map(|stream| {
+                stream["format"]
+                    .as_array()
+                    .unwrap()
+                    .iter()
+                    .filter(|&f| f["format_name"].as_str() != Some("flv"))
+                    .flat_map(|f| {
+                        f["codec"].as_array().unwrap().iter().flat_map(|codec| {
+                            let base_url = codec["base_url"].as_str().unwrap();
+                            let url_info = codec["url_info"].as_array().unwrap();
+                            url_info.iter().map(move |u| {
+                                let host = u["host"].as_str().unwrap();
+                                let extra = u["extra"].as_str().unwrap();
+                                format!("{}{}{}", host, base_url, extra)
+                            })
+                        })
+                    })
+            })
             .collect::<Vec<_>>();
-        Ok(urls)
+        Ok(res)
     }
 }
 
@@ -58,7 +78,7 @@ mod tests {
     async fn test() -> anyhow::Result<()> {
         let client = reqwest::Client::builder().build()?;
         // let room_id = 21743919;
-        let room_id = 1;
+        let room_id = 21507954;
         let s = StreamRoom::new(room_id, client, 10000);
         let url = s.get_url().await?;
         println!("{}", url);
