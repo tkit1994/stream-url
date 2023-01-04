108ab2fd067eee7af59118d923c676ee5957f106
HaoCheng <ch1994@outlook.com>
2023-01-04 19:38:18 +0800

    refactor change room id to string

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -40,23 +40,31 @@ pub enum StreamRoom {
 }
 
 impl StreamRoom {
-    pub fn new(platform: &str, room_id: u64, client: reqwest::Client) -> StreamRoom {
+    pub fn new(platform: &str, room_id: &str, client: reqwest::Client) -> StreamRoom {
         match platform.to_lowercase().as_str() {
             "douyu" => {
+                let room_id = room_id.parse().unwrap();
                 let room = douyu::StreamRoom::new(room_id, client);
                 StreamRoom::Douyu(room)
             }
             "huya" => {
+                let room_id = room_id.parse().unwrap();
                 let room = huya::StreamRoom::new(room_id, client);
                 StreamRoom::Huya(room)
             }
             "bilibili" => {
+                let room_id = room_id.parse().unwrap();
                 let room = bilibili::StreamRoom::new(room_id, client, 10000);
                 StreamRoom::Bilibili(room)
             }
             "douyin" => {
+                let room_id = room_id.parse().unwrap();
                 let room = douyin::StreamRoom::new(room_id, client);
                 StreamRoom::Douyin(room)
+            }
+            "twitch" => {
+                let room = twitch::StreamRoom::new(room_id, client);
+                StreamRoom::Twitch(room)
             }
             _ => StreamRoom::Unknownplatform,
         }
@@ -70,6 +78,7 @@ impl GetUrls for StreamRoom {
             StreamRoom::Huya(room) => room.get_urls().await,
             StreamRoom::Bilibili(room) => room.get_urls().await,
             StreamRoom::Douyin(room) => room.get_urls().await,
+            StreamRoom::Twitch(room) => room.get_urls().await,
             _ => bail!("Unknownplatform"),
         }
     }
@@ -81,7 +90,7 @@ mod tests {
     #[tokio::test]
     async fn test() -> anyhow::Result<()> {
         let client = reqwest::Client::builder().build()?;
-        let room_id = 6556593;
+        let room_id = "6556593";
         let room = StreamRoom::new("douyu", room_id, client);
         let url = room.get_url().await?;
         println!("{}", url);
