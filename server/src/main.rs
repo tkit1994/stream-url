2cec3465472b7368a48886434d202d354e3ea71a
HaoCheng <ch1994@outlook.com>
2023-01-06 13:04:51 +0800

    server: add api to get all urls

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,10 @@
 use axum::{
     extract::{Json, Path, Query, State},
     response::Redirect,
-    routing::get,
+    routing::{get, post},
 };
 
-use backend::GetUrl;
+use backend::{GetUrl, GetUrls};
 use clap::Parser;
 use error::AppError;
 use serde::{Deserialize, Serialize};
@@ -32,6 +32,15 @@ async fn post_url(
     Ok(url)
 }
 
+async fn get_all_urls(
+    State(client): State<reqwest::Client>,
+    Json(StreamRoomQuery { platform, room_id }): Json<StreamRoomQuery>,
+) -> Result<Json<Vec<String>>, AppError> {
+    let stream_room = backend::StreamRoom::new(platform.as_str(), room_id.as_str(), client);
+    let urls = stream_room.get_urls().await?;
+    Ok(Json(urls))
+}
+
 async fn get_url(
     State(client): State<reqwest::Client>,
     Query(StreamRoomQuery { platform, room_id }): Query<StreamRoomQuery>,
@@ -57,6 +66,7 @@ async fn main() -> anyhow::Result<()> {
     let app = axum::Router::new()
         .route("/", get(|| async { "hello from index" }))
         .route("/api/v1/stream/url", get(get_url).post(post_url))
+        .route("/api/v1/stream/all_urls", post(get_all_urls))
         .route("/api/v1/stream/:platform/:room_id", get(redirect_url))
         .with_state(client);
     let addr = format!("{}:{}", args.addr, args.port).parse()?;
